d17ce08d66b51d5b5e075a99914a7f5ce87af667
Abhishek <au2297579@gmail.com>
2026-06-26 12:48:42 +0530

    data added file

--- index_2.js
+++ index_2.js
@@ -19,6 +19,38 @@ app.get("/students",(req,res)=>{
     })
 });
 
+app.post("/students",(req,res)=>{
+    const {id,name,city}=req.body;
+    const newStudent={id,name,city};
+    students.push(newStudent);
+    res.json({
+        message:"record added",
+        students:newStudent,
+        data:students
+    })
+
+});
+// data update
+app.put("/students/:id",(req,res)=>{
+    // const id = req.params.id;
+    const {id}=req.params;
+    const student = students.find(s => s.id==id);
+    // if student found = value==>
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student not found"
+        });
+    }
+    student.name = req.body.name;
+    student.city= req.body.city;
+    res.json({
+        message:"record updated",
+        student
+    });
+});
+// app.delete
+
 app.listen(3000,()=>{
     console.log("server started");
     
